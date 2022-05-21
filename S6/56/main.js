5dd0e503a5744ff72bf608969b2558e17b7138e1
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-05-21 23:21:35 +0900

    56

---
+++ S6/56/main.js
@@ -0,0 +1,18 @@
+const person = {
+  name: ['john','ai'],
+  age: 32,
+  gender: 'male',
+  interests: {
+    sports: 'soccer',
+    music: 'piano'
+  },
+  getFullName: function(){
+    console.log(this.name[0] + this.name[1]);
+  }
+};
+
+
+const ageKey = 'age'
+person[ageKey] = 12;
+console.log(person.age);
+person.getFullName();
