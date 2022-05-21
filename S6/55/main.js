825692e88f98b078f2af8d3ca30a8a648189cf70
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-05-21 23:06:50 +0900

    55

---
+++ S6/55/main.js
@@ -0,0 +1,16 @@
+const array = [1,2,3,4,5,6,'moji',false]
+array[5] = 8;
+
+
+array.unshift('new item');
+const val = array.pop();
+const val2 = array.shift();
+console.log(val)
+console.log(val2);
+
+
+console.log(array);
+console.log(array.length);
+
+
+
