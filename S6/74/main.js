a000c4e25fa02c230f2f1611fb4a3a04ee5d254a
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-05-23 19:46:16 +0900

    74

---
+++ S6/74/main.js
@@ -0,0 +1,31 @@
+const obj = {
+  first_name: 'ai',
+  last_name: 'yosikawa',
+  printFullName: function() {
+    console.log(this.first_name + this.last_name);
+    window.setTimeout(function() {
+      console.log(this);
+    })
+  }
+}
+
+
+
+// console.log(obj.first_name);
+// obj.printFullName();
+
+class MyObj {
+  constructor() {
+    this.first_name = 'ai';
+    this.last_name = 'yosikawa';
+  }
+  printFullName() {
+    console.log(this.first_name + this.last_name);
+  }
+}
+
+const obj2 = new MyObj;
+
+obj.printFullName();
+obj2.printFullName();
+
