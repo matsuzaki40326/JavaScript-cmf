d3e70857b5c9c8a8d368f12d6ed1506f0d586046
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-05-23 16:08:42 +0900

    66

---
+++ S6/66/main.js
@@ -0,0 +1,21 @@
+const str = 'animation';
+const strArry = str.split('');
+console.log(strArry);
+
+function tag(accu, curr) {
+  console.log(accu, curr);
+  return `${accu}<${curr}>`;
+}
+
+
+function reduce (arry, callback, defaultValue) {
+  let accu = defaultValue;
+  for(let i = 0; i < arry.length; i++) {
+    let curr = arry[i];
+     accu = callback(accu, curr);
+  }
+  return accu;
+}
+
+const result = reduce(strArry,tag, "");
+console.log(result);
