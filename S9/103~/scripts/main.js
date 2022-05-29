f1d9d8c7385fe0b8353c720ca05c1b464d6e13f0
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-05-29 13:50:05 +0900

    116

--- S9/103~/scripts/main.js
+++ S9/103~/scripts/main.js
@@ -6,13 +6,24 @@ class Main {
     constructor() {
     this.header = document.querySelector('.header');
     this._observers = [];
-    this._scrollInit();
     this._init();
+    }
+
+    set observers(val) {
+        this._observers.push(val);
+    }
+
+    get observers() {
+        return this._observers;
     }
 
     _init() {
         new MobileMenu();
         this.hero = new HeroSlider('.swiper-container');
+        Pace.on('done', this._paceDone.bind(this));
+    }
+
+    _paceDone() {
         this._scrollInit();
     }
 
@@ -38,23 +49,31 @@ class Main {
             this.header.classList.add('triggered');
         }
     }
+
     _toggleSlideAnimation(el, inview) {
         if(inview) {
-            this.hero().start();
+            this.hero.start();
         }else {
-            this.hero().stop();
+            this.hero.stop();
         }
     }
 
+    _destroyObservers() {
+        this.observers.forEach(ob => {
+            ob.destroy();
+        });
+    }
+
+    destroy() {
+        this._destroyObservers();
+    }
+
     _scrollInit() {
-        this._observers.push(
-            new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false})
-        );
-        this._observers.push(
-            new ScrollObserver('.cover-slide', this._inviewAnimation)
-        );
-        new ScrollObserver('.tween-animate-title', this._textAnimation);
-        new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bind(this), {once: false});
+        this.observers = new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false});
+        this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation);
+        this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation);
+        this.observers = new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bind(this), {once: false});
+        console.log(this.observers);
     }
 }
 
