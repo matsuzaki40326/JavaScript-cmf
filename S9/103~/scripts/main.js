531d0a23eb0cae5f55731382dce6161a385e088e
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-05-29 15:01:58 +0900

    117

--- S9/103~/scripts/main.js
+++ S9/103~/scripts/main.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 class Main {
     constructor() {
     this.header = document.querySelector('.header');
+    this.sides = document.querySelectorAll('.side');
     this._observers = [];
     this._init();
     }
@@ -27,14 +28,14 @@ class Main {
         this._scrollInit();
     }
 
-    _textAnimation = function (el, isIntersecting) {
+    _textAnimation(el, isIntersecting) {
         if(isIntersecting) {
             const ta = new TweenTextAnimation(el);
             ta.animate();
         }
     }
 
-    _inviewAnimation = function(el, inview) {
+    _inviewAnimation(el, inview) {
         if(inview) {
             el.classList.add('inview');
         }else {
@@ -47,6 +48,14 @@ class Main {
             this.header.classList.remove('triggered');
         }else {
             this.header.classList.add('triggered');
+        }
+    }
+
+    _sideAnimation(el, inview) {
+        if(inview) {
+            this.sides.forEach(side => side.classList.add('inview'));
+        }else {
+            this.sides.forEach(side => side.classList.remove('inview'));
         }
     }
 
@@ -71,9 +80,10 @@ class Main {
     _scrollInit() {
         this.observers = new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false});
         this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation);
-        this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation);
+        this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation, {rootMargin: "-200px 0px"});
         this.observers = new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bind(this), {once: false});
-        console.log(this.observers);
+        this.observers = new ScrollObserver('.appear', this._inviewAnimation);
+        this.observers = new ScrollObserver('#main-content', this._sideAnimation.bind(this), {once: false, rootMargin: "-300px 0px"});
     }
 }
 
